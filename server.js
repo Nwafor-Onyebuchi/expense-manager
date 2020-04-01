2bd8a1052cbc414721a137de44b65f5957940d65
Nwafor-Onyebuchi <buchilazarus4@gmail.com>
2020-04-01 13:09:20 +0100

    Rearrange project structure

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use(express.json())
 
 app.use('/api/v1/transactions', transactions)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT,  ()=>console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
